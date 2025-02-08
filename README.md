f94b59caf9043a8f485d5c4658e40ad4d9083d6b
whydenyscry <gogool124512@gmail.com>
2025-02-08 19:34:13 +0200

    add The Hyperchaotic Pang—Liu Attractor

--- README.md
+++ README.md
@@ -8,3 +8,40 @@ The plots are also available on Pinterest and Behance:
 
 <a href="https://www.pinterest.com/whydenyscry/when-math-and-art-came-face-to-face/" target="_blank"><img src="https://brandslogos.com/wp-content/uploads/images/large/pinterest-logo-black-and-white-4.png" style="width: 50px; height: 50px;"></a>
 <a href="https://www.behance.net/whydenyscry" target="_blank"><img src="https://www.iconsdb.com/icons/preview/white/behance-5-xxl.png" style="width: 50px; height: 50px;"></a>
+
+## The Hyperchaotic Pang—Liu Attractor
+**Reference:**\
+[Pang, S., & Liu, Y. (2011). A new hyperchaotic system from the Lü system and its control. Journal of Computational and Applied Mathematics, 235(8), 2775–2789.](https://doi.org/10.1016/j.cam.2010.11.029)
+
+$$ 
+\begin{cases}
+	\frac{\mathrm{d}x_1}{\mathrm{d}t} = \alpha\left(x_2-x_1\right)	\\
+	\frac{\mathrm{d}x_2}{\mathrm{d}t} = -x_1x_3+\varsigma x_2+x_4, \\
+	\frac{\mathrm{d}x_3}{\mathrm{d}t} = x_1x_2-\beta x_3, \\
+	\frac{\mathrm{d}x_4}{\mathrm{d}t} = -\delta x_1 -\varepsilon x_2
+\end{cases}
+$$
+
+$$
+\begin{bmatrix}
+	\alpha\\
+	\beta\\
+	\varsigma\\
+	\delta\\
+	\varepsilon
+\end{bmatrix} = \begin{bmatrix}
+	36\\
+	3\\
+	20\\
+	2\\
+	2
+\end{bmatrix}.
+$$
+
+<p align="center">
+  <img src="images_png/The_Hyperchaotic_Pang_Liu_Attractor_x1x2x3_projection.png"/>
+</p>
+
+<p align="center">
+  <img src="gifs/The_Hyperchaotic_Pang_Liu_Attractor_2D_projections.gif"/>
+</p>
---
+++ scripts/via .mat/TheHyperchaoticPangLiuAttractor.m
@@ -0,0 +1,56 @@
+% Pang, S., & Liu, Y. (2011). A new hyperchaotic system from the Lü system and its control. Journal of Computational and Applied Mathematics, 235(8), 2775–2789. doi:10.1016/j.cam.2010.11.029 
+
+clear
+close
+clc
+
+load("TheHyperchaoticPangLiuAttractor.mat")
+
+set(groot, "defaultAxesTickLabelInterpreter", "latex");
+set(groot, "defaultTextInterpreter", "latex");
+set(groot, "defaultLegendInterpreter", "latex");
+set(groot, "defaultColorbarTickLabelInterpreter", "latex");
+
+figure;
+scatter3(xsol(:, 1), xsol(:, 2), xsol(:, 3), 4, 1:length(t), 'filled');
+view([-136 24])
+colormap gray;
+colorbar;
+axis tight;
+grid on;
+box on;
+xlabel('$x_1$', 'FontSize', 14);
+ylabel('$x_2$', 'FontSize', 14);
+zlabel('$x_3$', 'FontSize', 14);
+title(["The Hyperchaotic Pang---Liu Attractor" "$x_1x_2x_3$-projection"], 'FontSize', 15.5);
+
+% exportgraphics(gcf, "images_png/The_Hyperchaotic_Pang_Liu_Attractor_x1x2x3_projection.png", 'Resolution', 1200)
+% exportgraphics(gcf, "images_pdf/The_Hyperchaotic_Pang_Liu_Attractor_x1x2x3_projection.pdf", "ContentType", "vector")
+% exportgraphics(gcf, "images_eps/The_Hyperchaotic_Pang_Liu_Attractor_x1x2x3_projection.eps", "ContentType", "vector")
+% print(gcf, "images_svg/The_Hyperchaotic_Pang_Liu_Attractor_x1x2x3_projection.svg", "-dsvg")
+% save("workspace/TheHyperchaoticPangLiuAttractor.mat")
+
+% combinations = nchoosek(1 : length(incond), 2);
+% videoFile = 'gifs/The_Hyperchaotic_Pang_Liu_Attractor_2D_projections.gif';
+% 
+% for i = 1 : length(combinations)
+%     figure;
+%     scatter(xsol(:, combinations(i, 1)), xsol(:, combinations(i, 2)), 4, 1:length(t), 'filled');
+%     title(["The Hyperchaotic Pang---Liu Attractor" sprintf("$x_%ix_%i$-projection",  combinations(i, 1),  combinations(i, 2))], 'FontSize', 15.5);
+%     xlabel(sprintf("$x_%i$", combinations(i, 1)), 'FontSize', 14);
+%     ylabel(sprintf("$x_%i$", combinations(i, 2)), 'FontSize', 14);
+%     colormap gray;
+%     colorbar;
+%     axis tight;
+%     grid on;
+%     box on;
+% 
+%     img_gray = rgb2gray(frame2im(getframe(gcf)));
+% 
+%     if i == 1
+%         imwrite(img_gray, videoFile, 'gif', 'LoopCount', inf, 'DelayTime', 1);
+%     else
+%         imwrite(img_gray, videoFile, 'gif', 'WriteMode', 'append', 'DelayTime', 1);
+%     end
+%     close(gcf);
+% end
